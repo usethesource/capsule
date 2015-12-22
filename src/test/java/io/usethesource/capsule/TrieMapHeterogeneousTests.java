70a726530e8f4f9fc33f7f5fb93a96bba93de3c2
Michael Steindorfer <Michael.Steindorfer@cwi.nl>
2015-12-22 11:16:36 +0100

    Updated specialized heterogeneous map and tests.

--- src/test/java/io/usethesource/capsule/TrieMapHeterogeneousTests.java
+++ src/test/java/io/usethesource/capsule/TrieMapHeterogeneousTests.java
@@ -9,6 +9,7 @@
  *******************************************************************************/
 package io.usethesource.capsule;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.math.BigInteger;
@@ -18,8 +19,8 @@ import org.junit.Test;
 
 public class TrieMapHeterogeneousTests {
 
-  // final static int size = 2097152;
-  final static int size = (int) Math.pow(2, 10);
+  final static int size = 2097152;
+  // final static int size = (int) Math.pow(2, 10);
 
   @Test
   public void testPrintStatsRandomSmallAndBigIntegers() {
@@ -44,26 +45,28 @@ public class TrieMapHeterogeneousTests {
         // System.out.println("BIG");
         bigCount++;
 
-        // TrieMap_Heterogeneous_BleedingEdge res =
-        // (TrieMap_Heterogeneous_BleedingEdge) map.__put(bigJ, bigJ);
-        // assert res.containsKey(bigJ);
-
         TrieMap_Heterogeneous_BleedingEdge res =
-            (TrieMap_Heterogeneous_BleedingEdge) map.__put(boxedJ, boxedJ);
+            (TrieMap_Heterogeneous_BleedingEdge) map.__put(bigJ, bigJ);
+        assert res.containsKey(bigJ);
 
+        // TrieMap_Heterogeneous_BleedingEdge res =
+        // (TrieMap_Heterogeneous_BleedingEdge) map.__put(boxedJ, boxedJ);
         // assertTrue(res.containsKey(boxedJ));
         // because of non-overlap constraint
-        assertTrue(res.containsKey(j) || res.containsKey(boxedJ));
+        // assertTrue(res.containsKey(j) || res.containsKey(boxedJ));
+        
         map = res;
       } else {
         // System.out.println("SMALL");
         smallCount++;
         TrieMap_Heterogeneous_BleedingEdge res =
             (TrieMap_Heterogeneous_BleedingEdge) map.__put(j, j);
-
+        assertTrue(res.containsKey(j));
+        
         // assertTrue(res.containsKey(j));
         // because of non-overlap constraint
-        assertTrue(res.containsKey(j) || res.containsKey(boxedJ));
+        // assertTrue(res.containsKey(j) || res.containsKey(boxedJ));
+        
         map = res;
       }
     }
@@ -80,5 +83,56 @@ public class TrieMapHeterogeneousTests {
         100. * map.size() / (smallCount + bigCount)));
     System.out.println();
   }
+  
+  static final int populationCountPattern01(int v) {
+    int c = (v & 0x55555555) & (((v >> 1) & 0x55555555) ^ 0x55555555);
+    c = (c & 0x33333333) + ((c >> 2) & 0x33333333);
+    c = (c & 0x0F0F0F0F) + ((c >> 4) & 0x0F0F0F0F);
+    c = (c & 0x00FF00FF) + ((c >> 8) & 0x00FF00FF);
+    c = (c & 0x0000FFFF) + ((c >> 16)& 0x0000FFFF);    
+    return c;
+  }  
+  
+  @Test
+  public void testPopulationCountPattern01() {
+    assertEquals(2, populationCountPattern01(0b010001));
+    assertEquals(3, populationCountPattern01(0b010101));
+    assertEquals(2, populationCountPattern01(0b011001));
+    assertEquals(2, populationCountPattern01(0b011101));
+  }
+  
+  static final int populationCountPattern10(int v) {
+    int c = ((v & 0x55555555) ^ 0x55555555) & ((v >> 1) & 0x55555555);
+    c = (c & 0x33333333) + ((c >> 2) & 0x33333333);
+    c = (c & 0x0F0F0F0F) + ((c >> 4) & 0x0F0F0F0F);
+    c = (c & 0x00FF00FF) + ((c >> 8) & 0x00FF00FF);
+    c = (c & 0x0000FFFF) + ((c >> 16)& 0x0000FFFF);    
+    return c;
+  }  
+  
+  @Test
+  public void testPopulationCountPattern10() {
+    assertEquals(2, populationCountPattern10(0b100010));
+    assertEquals(3, populationCountPattern10(0b101010));
+    assertEquals(2, populationCountPattern10(0b100110));
+    assertEquals(2, populationCountPattern10(0b101110));
+  }
 
+  static final int populationCountPattern11(int v) {
+    int c = (v & 0x55555555) & ((v >> 1) & 0x55555555);
+    c = (c & 0x33333333) + ((c >> 2) & 0x33333333);
+    c = (c & 0x0F0F0F0F) + ((c >> 4) & 0x0F0F0F0F);
+    c = (c & 0x00FF00FF) + ((c >> 8) & 0x00FF00FF);
+    c = (c & 0x0000FFFF) + ((c >> 16)& 0x0000FFFF);    
+    return c;
+  }  
+  
+  @Test
+  public void testPopulationCountPattern11() {
+    assertEquals(2, populationCountPattern11(0b110011));
+    assertEquals(3, populationCountPattern11(0b111111));
+    assertEquals(2, populationCountPattern11(0b110111));
+    assertEquals(2, populationCountPattern11(0b111011));
+  }
+    
 }
