a498e84517a9ebbc11792f8b9e962138b68d5ecf
Michael Steindorfer <Michael.Steindorfer@cwi.nl>
2016-08-25 14:03:07 +0200

    Updating missing/wrong license headers on this branch after merge.

--- src/main/java/io/usethesource/capsule/ImmutableSetMultimapAsImmutableSetView.java
+++ src/main/java/io/usethesource/capsule/ImmutableSetMultimapAsImmutableSetView.java
@@ -1,14 +1,10 @@
-/*******************************************************************************
- * Copyright (c) 2015 CWI
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+/**
+ * Copyright (c) Michael Steindorfer <Centrum Wiskunde & Informatica> and Contributors.
+ * All rights reserved.
  *
- * Contributors:
- *
- *   * Michael Steindorfer - Michael.Steindorfer@cwi.nl - CWI  
- *******************************************************************************/
+ * This file is licensed under the BSD 2-Clause License, which accompanies this project
+ * and is available under https://opensource.org/licenses/BSD-2-Clause.
+ */
 package io.usethesource.capsule;
 
 import java.util.Collection;
