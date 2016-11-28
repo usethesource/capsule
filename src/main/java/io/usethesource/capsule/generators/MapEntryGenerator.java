61ec6f9924ce505aa31a8d2f5a0c47dd940092a9
Michael Steindorfer <michael.steindorfer@cwi.nl>
2016-11-28 01:41:09 +0100

    Extending property based testing.

---
+++ src/main/java/io/usethesource/capsule/generators/MapEntryGenerator.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) Michael Steindorfer <Centrum Wiskunde & Informatica> and Contributors.
+ * All rights reserved.
+ *
+ * This file is licensed under the BSD 2-Clause License, which accompanies this project
+ * and is available under https://opensource.org/licenses/BSD-2-Clause.
+ */
+package io.usethesource.capsule.generators;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import com.pholser.junit.quickcheck.generator.ComponentizedGenerator;
+import com.pholser.junit.quickcheck.generator.GenerationStatus;
+import com.pholser.junit.quickcheck.generator.Size;
+import com.pholser.junit.quickcheck.random.SourceOfRandomness;
+
+import io.usethesource.capsule.api.deprecated.ImmutableSet;
+
+@SuppressWarnings({"rawtypes", "unchecked"})
+public abstract class MapEntryGenerator<T extends ImmutableSet>
+    extends ComponentizedGenerator<T> {
+
+  private Class<T> target;
+  private Size sizeRange;
+
+  public MapEntryGenerator(Class<T> target) {
+    super(target);
+    this.target = target;
+  }
+
+  private int size(SourceOfRandomness random, GenerationStatus status) {
+    return sizeRange != null ? random.nextInt(sizeRange.min(), sizeRange.max()) : status.size();
+  }
+
+  protected final T empty() {
+    try {
+      final Method persistentSetOfEmpty = target.getMethod("of");
+      return (T) persistentSetOfEmpty.invoke(null);
+    } catch (NoSuchMethodException | SecurityException | IllegalAccessException
+        | IllegalArgumentException | InvocationTargetException e) {
+      throw new RuntimeException();
+    }
+  }
+
+  @Override
+  public int numberOfNeededComponents() {
+    return 1;
+  }
+
+  @Override
+  public T generate(SourceOfRandomness random, GenerationStatus status) {
+    int size = size(random, status);
+
+    T items = empty();
+    for (int i = 0; i < size; ++i) {
+      Object item = componentGenerators().get(0).generate(random, status);
+      if (item != null)
+        items = (T) items.__insert(item);
+    }
+
+    return items;
+  }
+
+}
---
+++ src/test/java/io/usethesource/capsule/BasicSetMultimapPropertiesTest.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) Michael Steindorfer <Centrum Wiskunde & Informatica> and Contributors.
+ * All rights reserved.
+ *
+ * This file is licensed under the BSD 2-Clause License, which accompanies this project
+ * and is available under https://opensource.org/licenses/BSD-2-Clause.
+ */
+package io.usethesource.capsule;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+
+import org.junit.runner.RunWith;
+
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+
+import io.usethesource.capsule.api.deprecated.ImmutableSet;
+import io.usethesource.capsule.experimental.lazy.TrieSet_5Bits_LazyHashCode;
+
+@RunWith(JUnitQuickcheck.class)
+public class BasicSetMultimapPropertiesTest {
+
+  private final int DEFAULT_TRIALS = 1_000;
+
+  @Property(trials = DEFAULT_TRIALS)
+  public void sizeAfterBatchInsertion(java.util.Set<Integer> input) {
+    ImmutableSet<Integer> set = DefaultTrieSet.<Integer>of().__insertAll(input);
+    assertEquals(input.size(), set.size());
+  }
+
+  @Property(trials = DEFAULT_TRIALS)
+  public <K> void sizeEqualsJavaCollection(ImmutableSet<K> input) {
+    assertEquals(input.size(), new HashSet<K>(input).size());
+  }
+
+  @Property(trials = DEFAULT_TRIALS)
+  public <K> void sizeEqualsJavaCollection1(TrieSet_5Bits_LazyHashCode<K> input) {
+    assertEquals(input.size(), new HashSet<K>(input).size());
+  }
+
+  // @Property(trials = DEFAULT_TRIALS)
+  // public void sizeAfterInsertIncreasesByOne(ImmutableSet<Integer> input, Integer item) {
+  // assertEquals(input.size() + 1, input.__insert(item).size());
+  // }
+  //
+  // @Property(trials = DEFAULT_TRIALS)
+  // public void containsAfterInsert1(ImmutableSet<Integer> input, Integer item) {
+  // assumeFalse(input.contains(item));
+  // assertTrue(input.__insert(item).contains(item));
+  // }
+  //
+  // @Property(trials = DEFAULT_TRIALS)
+  // public void containsAfterInsert2(ImmutableSet<Integer> input, Integer item) {
+  // assumeTrue(input.contains(item));
+  // assertTrue(input.__insert(item).contains(item));
+  // }
+  //
+  // @Property(trials = DEFAULT_TRIALS)
+  // public void notContainedAfterRemove(ImmutableSet<Integer> input, Integer item) {
+  // assumeTrue(input.contains(item));
+  // assertFalse(input.__remove(item).contains(item));
+  // }
+
+}
