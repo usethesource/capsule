245dcbc9541b77868df42c6db96e3a8916e379fa
Michael Steindorfer <michael.steindorfer@cwi.nl>
2017-01-10 18:34:05 +0100

    Add license information.

--- src/main/java/io/usethesource/capsule/api/deprecated/Set.java
+++ src/main/java/io/usethesource/capsule/api/deprecated/Set.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Michael Steindorfer <Centrum Wiskunde & Informatica> and Contributors.
+ * All rights reserved.
+ *
+ * This file is licensed under the BSD 2-Clause License, which accompanies this project
+ * and is available under https://opensource.org/licenses/BSD-2-Clause.
+ */
 package io.usethesource.capsule.api.deprecated;
 
 import java.util.Collection;
