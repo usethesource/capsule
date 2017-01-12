ea59768fad84418f54da31cc87cd912cc2b6532a
Michael Steindorfer <michael.steindorfer@cwi.nl>
2017-01-12 15:08:02 +0100

    Separating concern in interfaces.

--- src/main/java/io/usethesource/capsule/api/deprecated/Map.java
+++ src/main/java/io/usethesource/capsule/api/deprecated/Map.java
@@ -7,45 +7,32 @@
  */
 package io.usethesource.capsule.api.deprecated;
 
-import java.util.Comparator;
 import java.util.Iterator;
 
-public interface Map<K, V> extends java.util.Map<K, V> {
-  interface Immutable<K, V> extends Map<K, V> {
+public interface Map<K, V> extends java.util.Map<K, V>, MapEq<K, V> {
 
-    @Override
-    V get(final Object o);
+  @Override
+  V get(final Object o);
 
-    V getEquivalent(final Object o, final Comparator<Object> cmp);
+  @Override
+  boolean containsKey(final Object o);
 
-    @Override
-    boolean containsKey(final Object o);
+  @Override
+  boolean containsValue(final Object o);
 
-    boolean containsKeyEquivalent(final Object o, final Comparator<Object> cmp);
+  Iterator<K> keyIterator();
 
-    @Override
-    boolean containsValue(final Object o);
+  Iterator<V> valueIterator();
 
-    boolean containsValueEquivalent(final Object o, final Comparator<Object> cmp);
+  Iterator<Entry<K, V>> entryIterator();
+
+  interface Immutable<K, V> extends Map<K, V>, MapEq.Immutable<K, V> {
 
     Map.Immutable<K, V> __put(final K key, final V val);
 
-    Map.Immutable<K, V> __putEquivalent(final K key, final V val, final Comparator<Object> cmp);
-
-    Map.Immutable<K, V> __putAll(final java.util.Map<? extends K, ? extends V> map);
-
-    Map.Immutable<K, V> __putAllEquivalent(final java.util.Map<? extends K, ? extends V> map,
-                                       final Comparator<Object> cmp);
-
     Map.Immutable<K, V> __remove(final K key);
 
-    Map.Immutable<K, V> __removeEquivalent(final K key, final Comparator<Object> cmp);
-
-    Iterator<K> keyIterator();
-
-    Iterator<V> valueIterator();
-
-    Iterator<Entry<K, V>> entryIterator();
+    Map.Immutable<K, V> __putAll(final java.util.Map<? extends K, ? extends V> map);
 
     boolean isTransientSupported();
 
@@ -53,40 +40,13 @@ public interface Map<K, V> extends java.util.Map<K, V> {
 
   }
 
-  interface Transient<K, V> extends Map<K, V> {
-
-    @Override
-    V get(final Object o);
-
-    V getEquivalent(final Object o, final Comparator<Object> cmp);
-
-    @Override
-    boolean containsKey(final Object o);
-
-    boolean containsKeyEquivalent(final Object o, final Comparator<Object> cmp);
-
-    @Override
-    boolean containsValue(final Object o);
-
-    boolean containsValueEquivalent(final Object o, final Comparator<Object> cmp);
+  interface Transient<K, V> extends Map<K, V>, MapEq.Transient<K, V> {
 
     V __put(final K key, final V val);
 
-    V __putEquivalent(final K key, final V val, final Comparator<Object> cmp);
-
-    boolean __putAll(final java.util.Map<? extends K, ? extends V> map);
-
-    boolean __putAllEquivalent(final java.util.Map<? extends K, ? extends V> map, final Comparator<Object> cmp);
-
     V __remove(final K key);
 
-    V __removeEquivalent(final K key, final Comparator<Object> cmp);
-
-    Iterator<K> keyIterator();
-
-    Iterator<V> valueIterator();
-
-    Iterator<Entry<K, V>> entryIterator();
+    boolean __putAll(final java.util.Map<? extends K, ? extends V> map);
 
     Map.Immutable<K, V> freeze();
 
--- src/main/java/io/usethesource/capsule/api/deprecated/Set.java
+++ src/main/java/io/usethesource/capsule/api/deprecated/Set.java
@@ -8,74 +8,37 @@
 package io.usethesource.capsule.api.deprecated;
 
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.Iterator;
 
-public interface Set<K> extends java.util.Set<K> {
-  interface Immutable<K> extends Set<K> {
+public interface Set<K> extends java.util.Set<K>, SetEq<K> {
 
-    @Override
-    boolean containsAll(final Collection<?> c);
+  @Override
+  int size();
 
-    @Deprecated
-    default boolean containsAllEquivalent(final Collection<?> c, final Comparator<Object> cmp) {
-      throw new UnsupportedOperationException("Not yet implemented @ Set.Immutable.");
-    }
+  @Override
+  boolean isEmpty();
 
-    K get(final Object o);
+  @Override
+  boolean containsAll(Collection<?> c);
 
-    @Deprecated
-    default K getEquivalent(final Object o, final Comparator<Object> cmp) {
-      throw new UnsupportedOperationException("Not yet implemented @ Set.Immutable.");
-    }
+  K get(Object o);
 
-    @Override
-    boolean contains(final Object o);
+  @Override
+  boolean contains(Object o);
 
-    @Deprecated
-    default boolean containsEquivalent(final Object o, final Comparator<Object> cmp) {
-      throw new UnsupportedOperationException("Not yet implemented @ Set.Immutable.");
-    }
+  Iterator<K> keyIterator();
+
+  interface Immutable<K> extends Set<K>, SetEq.Immutable<K> {
 
     Set.Immutable<K> __insert(final K key);
 
-    @Deprecated
-    default Set.Immutable<K> __insertEquivalent(final K key, final Comparator<Object> cmp) {
-      throw new UnsupportedOperationException("Not yet implemented @ Set.Immutable.");
-    }
+    Set.Immutable<K> __remove(final K key);
 
     Set.Immutable<K> __insertAll(final java.util.Set<? extends K> set);
 
-    @Deprecated
-    default Set.Immutable<K> __insertAllEquivalent(final java.util.Set<? extends K> set,
-                                               final Comparator<Object> cmp) {
-      throw new UnsupportedOperationException("Not yet implemented @ Set.Immutable.");
-    }
-
-    Set.Immutable<K> __remove(final K key);
-
-    @Deprecated
-    default Set.Immutable<K> __removeEquivalent(final K key, final Comparator<Object> cmp) {
-      throw new UnsupportedOperationException("Not yet implemented @ Set.Immutable.");
-    }
-
     Set.Immutable<K> __removeAll(final java.util.Set<? extends K> set);
 
-    @Deprecated
-    default Set.Immutable<K> __removeAllEquivalent(final java.util.Set<? extends K> set,
-                                               final Comparator<Object> cmp) {
-      throw new UnsupportedOperationException("Not yet implemented @ Set.Immutable.");
-    }
-
     Set.Immutable<K> __retainAll(final java.util.Set<? extends K> set);
-
-    @Deprecated
-    default Set.Immutable<K> __retainAllEquivalent(final Set.Transient<? extends K> transientSet,
-                                               final Comparator<Object> cmp) {
-      throw new UnsupportedOperationException("Not yet implemented @ Set.Immutable.");
-    }
-
-    Iterator<K> keyIterator();
 
     boolean isTransientSupported();
 
@@ -83,68 +46,17 @@ public interface Set<K> extends java.util.Set<K> {
 
   }
 
-  interface Transient<K> extends Set<K> {
-
-    @Override
-    boolean containsAll(final Collection<?> c);
-
-    @Deprecated
-    default boolean containsAllEquivalent(final Collection<?> c, final Comparator<Object> cmp) {
-      throw new UnsupportedOperationException("Not yet implemented @ Set.Transient.");
-    }
-
-    K get(final Object o);
-
-    @Deprecated
-    default K getEquivalent(final Object o, final Comparator<Object> cmp) {
-      throw new UnsupportedOperationException("Not yet implemented @ Set.Transient.");
-    }
-
-    @Override
-    boolean contains(final Object o);
-
-    @Deprecated
-    default boolean containsEquivalent(final Object o, final Comparator<Object> cmp) {
-      throw new UnsupportedOperationException("Not yet implemented @ Set.Transient.");
-    }
+  interface Transient<K> extends Set<K>, SetEq.Transient<K> {
 
     boolean __insert(final K key);
 
-    @Deprecated
-    default boolean __insertEquivalent(final K key, final Comparator<Object> cmp) {
-      throw new UnsupportedOperationException("Not yet implemented @ Set.Transient.");
-    }
+    boolean __remove(final K key);
 
     boolean __insertAll(final java.util.Set<? extends K> set);
 
-    @Deprecated
-    default boolean __insertAllEquivalent(final java.util.Set<? extends K> set, final Comparator<Object> cmp) {
-      throw new UnsupportedOperationException("Not yet implemented @ Set.Transient.");
-    }
-
-    boolean __remove(final K key);
-
-    @Deprecated
-    default boolean __removeEquivalent(final K key, final Comparator<Object> cmp) {
-      throw new UnsupportedOperationException("Not yet implemented @ Set.Transient.");
-    }
-
     boolean __removeAll(final java.util.Set<? extends K> set);
 
-    @Deprecated
-    default boolean __removeAllEquivalent(final java.util.Set<? extends K> set, final Comparator<Object> cmp) {
-      throw new UnsupportedOperationException("Not yet implemented @ Set.Transient.");
-    }
-
     boolean __retainAll(final java.util.Set<? extends K> set);
-
-    @Deprecated
-    default boolean __retainAllEquivalent(final Set.Transient<? extends K> transientSet,
-                                          final Comparator<Object> cmp) {
-      throw new UnsupportedOperationException("Not yet implemented @ Set.Transient.");
-    }
-
-    Iterator<K> keyIterator();
 
     Set.Immutable<K> freeze();
 
