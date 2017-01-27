96077caa4519259f69cf4c1f18f7db9f958699dc
Michael Steindorfer <michael@steindorfer.name>
2017-01-27 12:15:51 +0100

    Separate test file.

---
+++ src/test/java/io/usethesource/capsule/SetMultimapPropertiesOfHhamtTest.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Michael Steindorfer <Centrum Wiskunde & Informatica> and Contributors.
+ * All rights reserved.
+ *
+ * This file is licensed under the BSD 2-Clause License, which accompanies this project
+ * and is available under https://opensource.org/licenses/BSD-2-Clause.
+ */
+package io.usethesource.capsule;
+
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import io.usethesource.capsule.experimental.multimap.TrieSetMultimap_HHAMT;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitQuickcheck.class)
+public class SetMultimapPropertiesOfHhamtTest extends
+    AbstractSetMultimapProperties<Integer, Integer, TrieSetMultimap_HHAMT<Integer, Integer>> {
+
+  public SetMultimapPropertiesOfHhamtTest() {
+    super(TrieSetMultimap_HHAMT.class);
+  }
+
+}
