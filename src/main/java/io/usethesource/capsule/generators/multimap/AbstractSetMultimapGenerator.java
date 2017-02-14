779df73fc44a16e5ea1e40e70dfa842bd01729ac
Michael Steindorfer <michael@steindorfer.name>
2017-02-14 15:45:26 +0100

    Adding support for generation of 1:n multi-map data.

--- src/main/java/io/usethesource/capsule/generators/multimap/AbstractSetMultimapGenerator.java
+++ src/main/java/io/usethesource/capsule/generators/multimap/AbstractSetMultimapGenerator.java
@@ -7,10 +7,6 @@
  */
 package io.usethesource.capsule.generators.multimap;
 
-import static com.pholser.junit.quickcheck.internal.Ranges.checkRange;
-import static com.pholser.junit.quickcheck.internal.Ranges.Type.INTEGRAL;
-import static java.util.stream.StreamSupport.stream;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -18,8 +14,10 @@ import com.pholser.junit.quickcheck.generator.ComponentizedGenerator;
 import com.pholser.junit.quickcheck.generator.GenerationStatus;
 import com.pholser.junit.quickcheck.generator.Size;
 import com.pholser.junit.quickcheck.random.SourceOfRandomness;
-
 import io.usethesource.capsule.api.SetMultimap;
+
+import static com.pholser.junit.quickcheck.internal.Ranges.Type.INTEGRAL;
+import static com.pholser.junit.quickcheck.internal.Ranges.checkRange;
 
 @SuppressWarnings({"rawtypes", "unchecked"})
 public abstract class AbstractSetMultimapGenerator<T extends SetMultimap.Immutable>
@@ -62,10 +60,30 @@ public abstract class AbstractSetMultimapGenerator<T extends SetMultimap.Immutab
     int size = size(random, status);
 
     T items = empty();
-    for (int i = 0; i < size; ++i) {
-      Object item0 = componentGenerators().get(0).generate(random, status);
-      Object item1 = componentGenerators().get(1).generate(random, status);
-      items = (T) items.__insert(item0, item1);
+
+    final int oneToOneStepSize = 2;
+    final int oneToManySetSize = 2;
+
+    int i = size - 1;
+    while (i >= 0) {
+      final Object item0 = componentGenerators().get(0).generate(random, status);
+
+      final int generatedTuplesCount;
+      if (i % oneToOneStepSize == 0) {
+        final Object item1 = componentGenerators().get(1).generate(random, status);
+        items = (T) items.__insert(item0, item1);
+
+        generatedTuplesCount = 1;
+      } else {
+        for (int j = oneToManySetSize - 1; j >= 0 && i >= 0; j--) {
+          final Object item1 = componentGenerators().get(1).generate(random, status);
+          items = (T) items.__insert(item0, item1);
+        }
+
+        generatedTuplesCount = oneToManySetSize;
+      }
+
+      i = i - generatedTuplesCount;
     }
 
     return items;
@@ -76,46 +94,46 @@ public abstract class AbstractSetMultimapGenerator<T extends SetMultimap.Immutab
     return false;
   }
 
-//  @Override public List<T> doShrink(SourceOfRandomness random, T larger) {
-//    @SuppressWarnings("unchecked")
-//    List<Object> asList = new ArrayList<>(larger.entrySet());
-//
-//    List<T> shrinks = new ArrayList<>();
-//    shrinks.addAll(removals(asList));
-//
-//    @SuppressWarnings("unchecked")
-//    Shrink<Object> generator = (Shrink<Object>) componentGenerators().get(0);
-//
-//    List<List<Object>> oneItemShrinks = shrinksOfOneItem(random, asList, generator);
-//    shrinks.addAll(oneItemShrinks.stream()
-//        .map(this::convert)
-//        .filter(this::inSizeRange)
-//        .collect(Collectors.toList()));
-//
-//    return shrinks;
-//  }
+  // @Override public List<T> doShrink(SourceOfRandomness random, T larger) {
+  // @SuppressWarnings("unchecked")
+  // List<Object> asList = new ArrayList<>(larger.entrySet());
+  //
+  // List<T> shrinks = new ArrayList<>();
+  // shrinks.addAll(removals(asList));
+  //
+  // @SuppressWarnings("unchecked")
+  // Shrink<Object> generator = (Shrink<Object>) componentGenerators().get(0);
+  //
+  // List<List<Object>> oneItemShrinks = shrinksOfOneItem(random, asList, generator);
+  // shrinks.addAll(oneItemShrinks.stream()
+  // .map(this::convert)
+  // .filter(this::inSizeRange)
+  // .collect(Collectors.toList()));
+  //
+  // return shrinks;
+  // }
 
   private boolean inSizeRange(T items) {
     return sizeRange == null
         || (items.size() >= sizeRange.min() && items.size() <= sizeRange.max());
   }
 
-//  private List<T> removals(List<?> items) {
-//    return stream(halving(items.size()).spliterator(), false)
-//        .map(i -> removeFrom(items, i))
-//        .flatMap(Collection::stream)
-//        .map(this::convert)
-//        .filter(this::inSizeRange)
-//        .collect(Collectors.toList());
-//  }
-//
-//  @SuppressWarnings("unchecked")
-//  private T convert(List<?> items) {
-//    T converted = empty();
-//    for (Object item : items) {
-//      converted = (T) converted.__insert(item);
-//    }
-//    return converted;
-//  }
+  // private List<T> removals(List<?> items) {
+  // return stream(halving(items.size()).spliterator(), false)
+  // .map(i -> removeFrom(items, i))
+  // .flatMap(Collection::stream)
+  // .map(this::convert)
+  // .filter(this::inSizeRange)
+  // .collect(Collectors.toList());
+  // }
+  //
+  // @SuppressWarnings("unchecked")
+  // private T convert(List<?> items) {
+  // T converted = empty();
+  // for (Object item : items) {
+  // converted = (T) converted.__insert(item);
+  // }
+  // return converted;
+  // }
 
 }
