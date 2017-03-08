2db4d1a0037b4ee13326c3b9ff058a94da1fc92b
Michael Steindorfer <michael@steindorfer.name>
2017-03-08 14:09:05 +0100

    Adding factory methods.

--- capsule-core/src/main/java/io/usethesource/capsule/BinaryRelation.java
+++ capsule-core/src/main/java/io/usethesource/capsule/BinaryRelation.java
@@ -7,6 +7,8 @@
  */
 package io.usethesource.capsule;
 
+import io.usethesource.capsule.core.PersistentBidirectionalTrieSetMultimap;
+
 public interface BinaryRelation<T, U> extends SetMultimap<T, U> {
 
   BinaryRelation<U, T> inverse();
@@ -21,12 +23,154 @@ public interface BinaryRelation<T, U> extends SetMultimap<T, U> {
     @Override
     BinaryRelation.Transient<K, V> asTransient();
 
+    static <K, V> BinaryRelation.Immutable<K, V> of() {
+      return PersistentBidirectionalTrieSetMultimap.of();
+    }
+
+    static <K, V> BinaryRelation.Immutable<K, V> of(K key0, V value0) {
+      final BinaryRelation.Transient<K, V> tmp = BinaryRelation.Transient.of();
+
+      tmp.__insert(key0, value0);
+
+      return tmp.freeze();
+    }
+
+    static <K, V> BinaryRelation.Immutable<K, V> of(K key0, V value0, K key1, V value1) {
+      final BinaryRelation.Transient<K, V> tmp = BinaryRelation.Transient.of();
+
+      tmp.__insert(key0, value0);
+      tmp.__insert(key1, value1);
+
+      return tmp.freeze();
+    }
+
+    static <K, V> BinaryRelation.Immutable<K, V> of(K key0, V value0, K key1, V value1, K key2,
+        V value2) {
+      final BinaryRelation.Transient<K, V> tmp = BinaryRelation.Transient.of();
+
+      tmp.__insert(key0, value0);
+      tmp.__insert(key1, value1);
+      tmp.__insert(key2, value2);
+
+      return tmp.freeze();
+    }
+
+    static <K, V> BinaryRelation.Immutable<K, V> of(K key0, V value0, K key1, V value1, K key2,
+        V value2, K key3, V value3) {
+      final BinaryRelation.Transient<K, V> tmp = BinaryRelation.Transient.of();
+
+      tmp.__insert(key0, value0);
+      tmp.__insert(key1, value1);
+      tmp.__insert(key2, value2);
+      tmp.__insert(key3, value3);
+
+      return tmp.freeze();
+    }
+
+    static <K, V> BinaryRelation.Immutable<K, V> of(K key0, V value0, K key1, V value1, K key2,
+        V value2, K key3, V value3, K key4, V value4) {
+      final BinaryRelation.Transient<K, V> tmp = BinaryRelation.Transient.of();
+
+      tmp.__insert(key0, value0);
+      tmp.__insert(key1, value1);
+      tmp.__insert(key2, value2);
+      tmp.__insert(key3, value3);
+      tmp.__insert(key4, value4);
+
+      return tmp.freeze();
+    }
+
+    static <K, V> BinaryRelation.Immutable<K, V> of(K key0, V value0, K key1, V value1, K key2,
+        V value2, K key3, V value3, K key4, V value4, K key5, V value5) {
+      final BinaryRelation.Transient<K, V> tmp = BinaryRelation.Transient.of();
+
+      tmp.__insert(key0, value0);
+      tmp.__insert(key1, value1);
+      tmp.__insert(key2, value2);
+      tmp.__insert(key3, value3);
+      tmp.__insert(key4, value4);
+      tmp.__insert(key5, value5);
+
+      return tmp.freeze();
+    }
+
   }
 
   interface Transient<K, V> extends BinaryRelation<K, V>, SetMultimap.Transient<K, V> {
 
     @Override
     BinaryRelation.Immutable<K, V> freeze();
+
+    static <K, V> BinaryRelation.Transient<K, V> of() {
+      return PersistentBidirectionalTrieSetMultimap.transientOf();
+    }
+
+    static <K, V> BinaryRelation.Transient<K, V> of(K key0, V value0) {
+      final BinaryRelation.Transient<K, V> tmp = BinaryRelation.Transient.of();
+
+      tmp.__insert(key0, value0);
+
+      return tmp;
+    }
+
+    static <K, V> BinaryRelation.Transient<K, V> of(K key0, V value0, K key1, V value1) {
+      final BinaryRelation.Transient<K, V> tmp = BinaryRelation.Transient.of();
+
+      tmp.__insert(key0, value0);
+      tmp.__insert(key1, value1);
+
+      return tmp;
+    }
+
+    static <K, V> BinaryRelation.Transient<K, V> of(K key0, V value0, K key1, V value1, K key2,
+        V value2) {
+      final BinaryRelation.Transient<K, V> tmp = BinaryRelation.Transient.of();
+
+      tmp.__insert(key0, value0);
+      tmp.__insert(key1, value1);
+      tmp.__insert(key2, value2);
+
+      return tmp;
+    }
+
+    static <K, V> BinaryRelation.Transient<K, V> of(K key0, V value0, K key1, V value1, K key2,
+        V value2, K key3, V value3) {
+      final BinaryRelation.Transient<K, V> tmp = BinaryRelation.Transient.of();
+
+      tmp.__insert(key0, value0);
+      tmp.__insert(key1, value1);
+      tmp.__insert(key2, value2);
+      tmp.__insert(key3, value3);
+
+      return tmp;
+    }
+
+    static <K, V> BinaryRelation.Transient<K, V> of(K key0, V value0, K key1, V value1, K key2,
+        V value2, K key3, V value3, K key4, V value4) {
+      final BinaryRelation.Transient<K, V> tmp = BinaryRelation.Transient.of();
+
+      tmp.__insert(key0, value0);
+      tmp.__insert(key1, value1);
+      tmp.__insert(key2, value2);
+      tmp.__insert(key3, value3);
+      tmp.__insert(key4, value4);
+
+      return tmp;
+    }
+
+    static <K, V> BinaryRelation.Transient<K, V> of(K key0, V value0, K key1, V value1, K key2,
+        V value2, K key3, V value3, K key4, V value4, K key5, V value5) {
+      final BinaryRelation.Transient<K, V> tmp = BinaryRelation.Transient.of();
+
+      tmp.__insert(key0, value0);
+      tmp.__insert(key1, value1);
+      tmp.__insert(key2, value2);
+      tmp.__insert(key3, value3);
+      tmp.__insert(key4, value4);
+      tmp.__insert(key5, value5);
+
+      return tmp;
+    }
 
   }
 
