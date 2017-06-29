c72542075039a070ec88932d8241af158b1aa1ea
Michael Steindorfer <michael@steindorfer.name>
2017-06-29 11:36:41 +0200

    Adding particular entry point for PersistentTrieSet tests.

---
+++ capsule-veritas/src/test/java/io/usethesource/capsule/SetPropertiesOfPersistentTrieSetTest.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) Michael Steindorfer <Centrum Wiskunde & Informatica> and Contributors.
+ * All rights reserved.
+ *
+ * This file is licensed under the BSD 2-Clause License, which accompanies this project
+ * and is available under https://opensource.org/licenses/BSD-2-Clause.
+ */
+package io.usethesource.capsule;
+
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import io.usethesource.capsule.core.PersistentTrieSet;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitQuickcheck.class)
+public class SetPropertiesOfPersistentTrieSetTest
+    extends AbstractSetProperties<Integer, PersistentTrieSet<Integer>> {
+
+  public SetPropertiesOfPersistentTrieSetTest() {
+    super(PersistentTrieSet.class);
+  }
+}
