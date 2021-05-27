7d01709ee5308e23f5dadd2ba1a6cb11d190a824
Michael Steindorfer <msteindorfer@apple.com>
2021-05-27 16:04:10 +0200

    Move `RuntimeCodeGenerationTestSuite` files

---
+++ capsule-core/src/test/java/io/usethesource/capsule/RuntimeCodeGenerationTest.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) Michael Steindorfer <Centrum Wiskunde & Informatica> and Contributors.
+ * All rights reserved.
+ *
+ * This file is licensed under the BSD 2-Clause License, which accompanies this project
+ * and is available under https://opensource.org/licenses/BSD-2-Clause.
+ */
+package io.usethesource.capsule;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+
+@Ignore
+@RunWith(RuntimeCodeGenerationTestSuite.class)
+public class RuntimeCodeGenerationTest {
+}
