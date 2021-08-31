48741b33c4723cbf8de0f979320c92f9c3c63d94
Michael Steindorfer <msteindorfer@apple.com>
2021-08-31 18:22:52 +0200

    Add test cases for previously fixed multi-map hash-collision issues

--- src/test/java/io/usethesource/capsule/SetMultimapSmokeTest.java
+++ src/test/java/io/usethesource/capsule/SetMultimapSmokeTest.java
@@ -8,6 +8,7 @@
 package io.usethesource.capsule;
 
 import java.util.Collection;
+import java.util.Iterator;
 
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.generator.Size;
@@ -130,5 +131,122 @@ public class SetMultimapSmokeTest<K, V, CT extends SetMultimap.Immutable<K, V>> 
 
     assertNotEquals(map, mapDifferent);
     assertNotEquals(mapDifferent, map);
+  }
+
+  private int size(Iterator<?> iterator) {
+    int size = 0;
+
+    while (iterator.hasNext()) {
+      size++;
+      iterator.next();
+    }
+
+    return size;
+  }
+
+  @Property
+  public void testHashCollisionPut(
+      @Size(max = 0) final io.usethesource.capsule.core.PersistentTrieSetMultimap<Object, Integer> emptyCollection) { // TODO generic type `SetMultimap.Immutable` not applicable, since `PersistentBidirectionalTrieSetMultimap` doesn't support `__put(K key, Set.Immutable<V> values)`
+
+    Object a = new Object() {
+      public int hashCode() {
+        return 0;
+      }
+    };
+
+    Object b = new Object() {
+      public int hashCode() {
+        return 0;
+      }
+    };
+
+    Object c = new Object() {
+      public int hashCode() {
+        return 0;
+      }
+    };
+
+    final SetMultimap.Immutable<Object, Integer> mapWithBucketsOfSize3 = emptyCollection
+        .__insert(a, 1).__insert(a, 2).__insert(a, 3)
+        .__insert(b, 4).__insert(b, 5).__insert(b, 6)
+        .__insert(c, 7).__insert(c, 8).__insert(c, 9);
+
+    assertEquals(3, mapWithBucketsOfSize3.sizeDistinct());
+    assertEquals(9, mapWithBucketsOfSize3.size());
+    assertEquals(3, size(mapWithBucketsOfSize3.nativeEntryIterator()));
+    assertEquals(9, size(mapWithBucketsOfSize3.entryIterator()));
+
+    final SetMultimap.Immutable<Object, Integer> mapWithBucketsOfSize1 = mapWithBucketsOfSize3
+        .__put(a, 0)
+        .__put(b, 0)
+        .__put(c, 0);
+
+    assertEquals(3, mapWithBucketsOfSize1.sizeDistinct());
+    assertEquals(3, mapWithBucketsOfSize1.size());
+    assertEquals(3, size(mapWithBucketsOfSize1.nativeEntryIterator()));
+    assertEquals(3, size(mapWithBucketsOfSize1.entryIterator()));
+
+    final SetMultimap.Immutable<Object, Integer> mapWithBucketsOfSize2 = mapWithBucketsOfSize3
+        .__put(a, Set.Immutable.of(1, 2))
+        .__put(b, Set.Immutable.of(4, 5))
+        .__put(c, Set.Immutable.of(7, 8));
+
+    assertEquals(3, mapWithBucketsOfSize2.sizeDistinct());
+    assertEquals(6, mapWithBucketsOfSize2.size());
+    assertEquals(3, size(mapWithBucketsOfSize2.nativeEntryIterator()));
+    assertEquals(6, size(mapWithBucketsOfSize2.entryIterator()));
+  }
+
+  @Property
+  public void testHashCollisionInsert(
+      @Size(max = 0) final SetMultimap.Immutable<Object, Integer> emptyCollection) {
+
+    Object a = new Object() {
+      public int hashCode() {
+        return 0;
+      }
+    };
+
+    Object b = new Object() {
+      public int hashCode() {
+        return 0;
+      }
+    };
+
+    Object c = new Object() {
+      public int hashCode() {
+        return 0;
+      }
+    };
+
+    final SetMultimap.Immutable<Object, Integer> mapWithBucketsOfSize1 = emptyCollection
+        .__insert(a, 1)
+        .__insert(b, 4)
+        .__insert(c, 7);
+
+    assertEquals(3, mapWithBucketsOfSize1.sizeDistinct());
+    assertEquals(3, mapWithBucketsOfSize1.size());
+    assertEquals(3, size(mapWithBucketsOfSize1.nativeEntryIterator()));
+    assertEquals(3, size(mapWithBucketsOfSize1.entryIterator()));
+
+    final SetMultimap.Immutable<Object, Integer> mapWithBucketsOfSize2 = mapWithBucketsOfSize1
+        .__insert(a, Set.Immutable.of(2))
+        .__insert(b, Set.Immutable.of(5))
+        .__insert(c, Set.Immutable.of(8));
+
+    assertEquals(3, mapWithBucketsOfSize2.sizeDistinct());
+    assertEquals(6, mapWithBucketsOfSize2.size());
+    assertEquals(3, size(mapWithBucketsOfSize2.nativeEntryIterator()));
+    assertEquals(6, size(mapWithBucketsOfSize2.entryIterator()));
+
+    final SetMultimap.Immutable<Object, Integer> mapWithBucketsOfSize3 = mapWithBucketsOfSize1
+        .__insert(a, Set.Immutable.of(2, 3))
+        .__insert(b, Set.Immutable.of(5, 6))
+        .__insert(c, Set.Immutable.of(8, 9));
+
+    assertEquals(3, mapWithBucketsOfSize3.sizeDistinct());
+    assertEquals(9, mapWithBucketsOfSize3.size());
+    assertEquals(3, size(mapWithBucketsOfSize3.nativeEntryIterator()));
+    assertEquals(9, size(mapWithBucketsOfSize3.entryIterator()));
   }
 }
